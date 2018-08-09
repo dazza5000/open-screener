ef5dd4d294c36752e5a3529040a3c92c051d546f
Darran Kelinske <darran.kelinske@gmail.com>
2018-08-08 20:29:47 -0500

    hook up start visit view

---
+++ lib/view/home_view.dart
@@ -0,0 +1,62 @@
+import 'package:open_screener/view/start_visit_view.dart';
+import 'package:flutter/material.dart';
+
+String startVisitScreenTitle = "Start Visit";
+
+class HomeView extends StatefulWidget {
+  HomeView({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  HomeViewState createState() => new HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+
+  int currentTab = 0;
+  StartVisitView startVisitView = new StartVisitView();
+  List<Widget> pages;
+  Widget currentPage;
+
+  @override
+  void initState() {
+    super.initState();
+    pages = [startVisitView]; // Populate our pages list.
+    currentPage = startVisitView;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Here we create our BottomNavigationBar.
+    final BottomNavigationBar navBar = new BottomNavigationBar(
+      currentIndex: currentTab,
+      onTap: (int numTab) {
+        setState(() {
+          currentTab = numTab;
+          currentPage = pages[numTab];
+        });
+      },
+      items: <BottomNavigationBarItem>[
+        new BottomNavigationBarItem(
+            icon: new Icon(Icons.list), title: new Text(startVisitScreenTitle)),
+        new BottomNavigationBarItem(
+            icon: new Icon(Icons.map), title: new Text("Map")),
+      ],
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      bottomNavigationBar: navBar, // Assigning our navBar to the Scaffold's bottomNavigationBar property.
+      body: currentPage, // The body will be the currentPage. Which we update when a tab is pressed.
+    );
+  }
+}
---
+++ lib/view/start_visit_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class StartVisitView extends StatefulWidget {
+  @override
+  _StartVisitViewState createState() => new _StartVisitViewState();
+}
+
+class _StartVisitViewState extends State<StartVisitView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+ }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+          child: new Text("yeehaw"),
+          ));
+  }
