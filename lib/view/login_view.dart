2f568d6bcddc913b43f9980da3adbbe505b7f39e
Darran Kelinske <darran.kelinske@gmail.com>
2018-08-22 20:43:56 -0500

    fix bug in pubspec.yaml

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -15,7 +15,7 @@ class _LoginPageState extends State<LoginView> {
       child: CircleAvatar(
         backgroundColor: Colors.transparent,
         radius: 48.0,
-        child: Image.asset('assets/logo.png'),
+        child: Image.asset('assets/HH_Color.jpg'),
       ),
     );
 
