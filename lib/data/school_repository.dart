70687884bec1cdde1c22e1831cd2d66e6c747ff8
SamIAmHarris <SamHarris512@gmail.com>
2018-10-03 17:49:49 -0500

    Update the collection names for a few of the queries (#9)
    * Update the collection names for a few of the queries
    
    * Add user id to the query to make sure we only get schools where the user is a school admin.
    
    * minor clean up

--- lib/data/school_repository.dart
+++ lib/data/school_repository.dart
@@ -1,18 +1,26 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:open_screener/model/school.dart';
+import 'package:open_screener/util/auth_util.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-String schoolReference = "school";
+
+String schoolReference = "schools";
 String schoolNameField = "name";
+String schoolAdminField = "school_admin";
 
 class SchoolRepository {
   static Future<List<School>> getSchoolsFromFirestore() async {
     CollectionReference ref = Firestore.instance.collection(schoolReference);
+    final String userId = await AuthUtil.getUserId();
 
-    QuerySnapshot studentQuery =
-        await ref.orderBy(schoolNameField).getDocuments();
+    QuerySnapshot schoolQuery =
+        await ref
+            .where(schoolAdminField, isEqualTo: userId)
+            .orderBy(schoolNameField)
+            .getDocuments();
 
-    return studentQuery.documents.map((document) {
+    return schoolQuery.documents.map((document) {
       return new School(
         name: document.data[schoolNameField],
       );
